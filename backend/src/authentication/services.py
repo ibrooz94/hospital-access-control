0c6b5e47a5b5e85f9439632f16279975a077c1b4
ibrooz94 <ibrooz94@gmail.com>
2024-08-18 16:17:46 +0100

    feat: enable cross-site cookie transport

--- backend/src/authentication/services.py
+++ backend/src/authentication/services.py
@@ -8,7 +8,7 @@ from src.core.config import settings
 
 
 # bearer_transport = BearerTransport(tokenUrl=f"{settings.API_V1_STR}/auth/jwt/login")
-cookie_transport = CookieTransport(cookie_max_age=3600)
+cookie_transport = CookieTransport(cookie_samesite="none")
 
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=settings.SECRET_KEY, lifetime_seconds=3600)
